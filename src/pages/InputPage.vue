4f97bea54a6c44056cfcba1dd1dcf6a5a7ba34d8
FarrewX <a0987142276@gmail.com>
2023-10-10 16:22:25 +0700

    push in github

---
+++ src/pages/InputPage.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="htmlClass">
+    <h1>{{ title }}</h1>
+    <form>
+      <input type="text" id="เจษฎา" v-model="firstName" />
+      <input type="text" id="พรมโสภา (6504101314)" v-model="secondName" />
+      <button @click="submitForm()" class="submitForm">Submit</button>
+  </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'InputPage',
+  data() {
+    return {
+      title: 'Jesada Promsopa',
+      firstName: '',
+      secondName: '',
+    }
+  },
+  methods: {
+    submitForm() {
+      this.$emit('submit form1')
+    },
+  }
+}
+</script>
+
+<style scoped></style>
